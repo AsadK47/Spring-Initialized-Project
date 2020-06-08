e7f92e5a8b0da6f67f963d601bfb6d6f13a10d3f
Asad Khan <asadkkhan@deloitte.co.uk>
2020-06-08 12:33:40 +0100

    Started testing

--- src/test/java/com/deloitte/training/demo4/Demo4ApplicationTests.java
+++ src/test/java/com/deloitte/training/demo4/Demo4ApplicationTests.java
@@ -1,13 +1,47 @@
 package com.deloitte.training.demo4;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.hamcrest.Matchers.*;
 
 @SpringBootTest
 class Demo4ApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private static MediaType JSON_CONTENT_TYPE = MediaType.parseMediaType("application/json;charset=UTF-8");
+
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    public void setup() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(this.webApplicationContext).build();
+    }
+
+    @Test
+    void allCarsCanBeFound() throws Exception {
+    	mockMvc.perform(get("/cars").accept(JSON_CONTENT_TYPE))
+				.andExpect(jsonPath("$").isArray())
+				.andExpect(jsonPath("$", hasSize(8)));
+    }
 
 }
